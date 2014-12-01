08e9a53376ad9f930793bc15cc85d5c67a4f53d8
Eric Halpern <eric.halpern@gmail.com>
2014-11-30 18:27:03 -0800

    Doc changes

--- src/main/scala/twine/akka/AkkaModule.scala
+++ src/main/scala/twine/akka/AkkaModule.scala
@@ -8,6 +8,8 @@ import scala.concurrent.ExecutionContext
 
 /**
  * This module defines the bindings required to support Guice injectable Akka actors.
+ * It is a core module required to bootstrap the spray router for rest support.
+ *
  * This includes:
  *
  * - ActorSystem      - a singleton instance of the root actor system
@@ -27,8 +29,9 @@ class AkkaModule extends ScalaModule
   }
 
   /**
-   * Instantiates the root actor system and registers the GuiceAkkaExtension.
-   * The @Singleton annotation ensures only one instance is created.
+   * Provides the singleton root-actor-system to be injected whenever an ActorSystem
+   * is required.  This method also registers the GuiceAkkaExtension
+   * to be used for instantiating guice injected actors.
    */
   @Provides @Singleton
   def provideActorSystem(injector: Injector) : ActorSystem = {
@@ -39,8 +42,8 @@ class AkkaModule extends ScalaModule
   }
 
   /**
-   * Binds ActorRefFactory to the root ActorSystem.  Note the the single binding
-   * to ActorSystem is not enough because Guice will only inject exact type matches
+   * Provides a singleton factory to be injected whenever an ActorRefFactory
+   * is required.
    */
   @Provides @Singleton
   def provideActorRefFactory(systemProvider: Provider[ActorSystem]): ActorRefFactory = {
@@ -48,7 +51,8 @@ class AkkaModule extends ScalaModule
   }
 
   /**
-   * Binds ExecutionContext to the root ActorSystem ExecutionContext.
+   * Provides a singleton execution context to be injected whenever an ExecutionContext
+   * is required.
    */
   @Provides @Singleton
   def provideExecutionContext(systemProvider: Provider[ActorSystem]): ExecutionContext = {
